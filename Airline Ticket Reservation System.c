868046a06148b7cfd2d861636c5a75632c5a2371
Syed M.Jafar Ali Naqvi <103287122+Sniper512@users.noreply.github.com>
2022-10-29 16:58:32 +0500

    Add files via upload

---
+++ Airline Ticket Reservation System.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+ 
+int main()
+{
+int seats[100]={0};
+int flag=1;
+int input=0;
+printf("\n***************Welcome to Airline Ticket Reservation System.******************\n\n");
+while(flag)
+{
+    printf("1)-Type 1 for Ticket in First Class.\n");
+    printf("2)-Type 2 for Ticket in Economy Class.\n");
+    printf("3)-Type 3 to Print Ticket's Availibility Chart.\n");
+    printf("3)-Exit\n");
+    printf("Choose from above: ");
+    scanf("%d",&input);
+    switch (input)
+    {
+    case 1:
+        
+        break;
+    
+    default:
+        break;
+    }
+
+}    
+    
+    
+    
+    
+return 0;
+}
