92bfbe63492b747c34f0d9addfe446121ff558c2
Syed M.Jafar Ali Naqvi <103287122+Sniper512@users.noreply.github.com>
2022-10-29 21:00:25 +0500

    Commit testing
    We are testing commit

--- Airline_Ticket_Reservation_System.c
+++ Airline_Ticket_Reservation_System.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
+
+void printTicket(int seat)
+{
+    printf("\n------------------------------------------\n");
+    printf("Ticket Booked.\n");
+    printf("Ticket Number:  %d\n", seat + 1);
+    printf("Ticket Class: ");
+    if (seat >= 0 && seat <= 29)
+        printf("First Class.\n");
+    else
+    {
+        printf("Economy Class.\n");
+    }
+    printf("------------------------------------------\n");
+}
 void FirstClass(int seats[], int size)
 {
     int num = 0;
@@ -11,7 +26,15 @@ void FirstClass(int seats[], int size)
     else
     {
         num--;
+        if(seats[num] == 0)
+        {
         seats[num] = 1;
+        printTicket(num);
+        }
+        else{
+            printf("Seat is already Booked.Please select another one.\n");
+        }
+        
     }
 }
 void EconomyClass(int seats[], int size)
@@ -26,7 +49,14 @@ void EconomyClass(int seats[], int size)
     else
     {
         num--;
+        if(seats[num] == 0)
+        {
         seats[num] = 1;
+        printTicket(num);
+        }
+        else{
+            printf("Seat is already Booked.Please select another one.\n");
+        }
     }
 }
 void Ticket(int seats[], int size)
@@ -47,15 +77,15 @@ void Ticket(int seats[], int size)
         printf("Wrong input.\n");
     }
 }
-void Display(int seats[],int size)
+void Display(int seats[], int size)
 {
-    for(int i=0;i<size;i++)
+    for (int i = 0; i < size; i++)
     {
-        if(i%10==0)
+        if (i % 10 == 0)
         {
             printf("\n");
         }
-      printf("%d ",seats[i]);   
+        printf("%d ", seats[i]);
     }
 }
 int main()
@@ -63,9 +93,10 @@ int main()
     int seats[100] = {0};
     int flag = 1;
     int input = 0;
-    printf("\n***************Welcome to Airline Ticket Reservation System.******************\n");
+    printf("\n***************  Welcome to Airline Ticket Reservation System.  ******************\n");
     while (flag)
     {
+        printf("\n-----------------------------------------");
         printf("\n1)-Book a Ticket.\n");
         printf("2)-Print Ticket's Availibility Chart.\n");
         printf("3)-Exit\n");
@@ -77,8 +108,11 @@ int main()
             Ticket(seats, 100);
             break;
         case 2:
-        Display(seats,100);
-        break;
+            Display(seats, 100);
+            break;
+        case 3:
+            flag = 0;
+            break;
         default:
             break;
         }
